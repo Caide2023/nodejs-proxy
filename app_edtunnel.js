8b7b2a5a305704ecc91d56a48c91a7995aeb5651
Caide2023 <148255773+Caide2023@users.noreply.github.com>
2023-10-24 23:31:38 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '468a1c1a-f9f3-469f-aa31-36ed9dc70b20';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
